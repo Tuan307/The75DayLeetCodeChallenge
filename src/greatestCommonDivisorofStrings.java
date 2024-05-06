572fea4e2f8ed40b438cae335a42087ad0cd75e6
Pham Tuan <tuanprokt44@gmail.com>
2024-05-06 14:01:35 +0700

    leetcode 75 1071. Greatest Common Divisor of Strings

---
+++ src/greatestCommonDivisorofStrings.java
@@ -0,0 +1,22 @@
+public class greatestCommonDivisorofStrings {
+
+    public static String gcdOfStrings(String str1, String str2) {
+
+        String errorStr = str1;
+        if (str2.length() > str1.length()) {
+            return gcdOfStrings(str2, str1);
+        }
+        if (str1.equals(str2)) {
+            return str1;
+        }
+        if (str1.startsWith(str2)) {
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+        }
+        return "";
+    }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcdOfStrings("ABABAB", "ABAB"));
+    }
+}
